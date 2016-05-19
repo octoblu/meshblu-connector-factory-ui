ec3f0b2abcc6bd1f8c2ae1900d10c8701e898e92
sqrt <sqrt@octoblu.com>
2016-05-19 08:37:43 -0700

    v2.2.0 Add netscaler connector

--- src/assets/new-connectors.js
+++ src/assets/new-connectors.js
@@ -1,13 +1,19 @@
 export const newConnectors = [
   {
     _id: '9811c66d-2211-472a-9be0-e79cc397ce21',
-    name: "Lightblue Bean",
+    name: 'Lightblue Bean',
     type: 'device:bean',
     connector: 'meshblu-connector-bean'
   },
   {
+    _id: '1840f9c0-938c-4e71-917c-430030c751fd',
+    name: 'Netscaler',
+    type: 'device:netscaler',
+    connector: 'meshblu-connector-netscaler'
+  },
+  {
     _id: '12942439-f605-487d-b1b8-63dec418a248',
-    name: "Say Hello (For Testing)",
+    name: 'Say Hello (For Testing)',
     type: 'device:say-hello',
     connector: 'meshblu-connector-say-hello'
   },

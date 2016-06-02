f99740a34eecdb29e0f1a584fa8726af2eeb62dd
Sqrt of Saturn <sqrt@octoblu.com>
2016-06-02 15:56:27 -0700

    v2.8.5 removed arduino for now

--- src/assets/new-connectors.js
+++ src/assets/new-connectors.js
@@ -42,12 +42,6 @@ export const newConnectors = [
     connector: 'meshblu-connector-shell'
   },
   {
-    _id: 'efa1552d-0c60-472a-981a-23b856fd3b59',
-    name: 'Johnny-Five',
-    type: 'device:johnnyfive',
-    connector: 'meshblu-connector-arduino'
-  },
-  {
     _id: '5440604b-7062-4c2e-8f75-23b856fd3b59',
     name: 'Phillips Hue',
     type: 'device:hue',

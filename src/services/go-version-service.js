3b983ae175a21f50de1a75e7e6155d3670c22d52
Roy van de Water <r@octoblu.com>
2016-12-06 08:13:19 -0700

    v9.5.7 Release SRV for iginition

--- src/services/go-version-service.js
+++ src/services/go-version-service.js
@@ -1,7 +1,7 @@
 import async from 'async'
 
 export function getLatestIgnitionVersion(callback) {
-  callback(null, 'v8.1.0')
+  callback(null, 'v8.2.0')
 }
 
 export function getLatestInstallerVersion(callback) {

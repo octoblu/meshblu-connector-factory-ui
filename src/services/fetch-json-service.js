f297eb257c595ba0fc89d2a77c7d0f2fa29adc19
Jade Meskill <jade.meskill@gmail.com>
2017-07-31 13:17:55 -0700

    v11.0.6 handle 4xx errors gracefully

--- src/services/fetch-json-service.js
+++ src/services/fetch-json-service.js
@@ -1,26 +1,29 @@
 import request from 'superagent'
 
 export function getJSON({ uri }, callback) {
-  request.get(uri)
-    .end((error, response) => {
-      if (error) {
-        callback(error)
+  request.get(uri).end((error, response) => {
+    if (error) {
+      callback(error)
+      return
+    }
+    if (response.clientError) {
+      callback(null, {})
+      return
+    }
+    if (!response.ok) {
+      callback(new Error('Invalid Response'))
+      return
+    }
+    const { body, text } = response
+    let jsonResponse = body
+    if (!body && text) {
+      try {
+        jsonResponse = JSON.parse(text)
+      } catch (error) {
+        callback(new Error('Unable to Parse JSON'))
         return
       }
-      if (!response.ok) {
-        callback(new Error('Invalid Response'))
-        return
-      }
-      const { body, text } = response
-      let jsonResponse = body
-      if (!body && text) {
-        try {
-          jsonResponse = JSON.parse(text)
-        } catch (error) {
-          callback(new Error('Unable to Parse JSON'))
-          return
-        }
-      }
-      callback(null, jsonResponse)
-    })
+    }
+    callback(null, jsonResponse)
+  })
 }
